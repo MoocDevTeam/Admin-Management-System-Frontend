446baad0cd89b083eda60a58ee15bc4242642864
“Yang <changzzz@hotmail.com>
2025-01-12 15:57:59 +1100

    MoocUI 1.0.3: Yang Course back button

--- src/pages/course/course/courseList/index.jsx
+++ src/pages/course/course/courseList/index.jsx
@@ -64,7 +64,7 @@ export default function CourseList() {
           {courses.map((course) => (
             <Link
               key={course.courseCode}
-              to={`/course/${course.title}`}
+              to={`/course/${course.id}`}
               style={{ textDecoration: "none" }}
             >
               <CourseCard
