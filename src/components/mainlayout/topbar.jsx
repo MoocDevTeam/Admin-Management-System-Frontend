acd7082d09b9cdba65ae9917072433df2f6ad868
DESKTOP-LLHTIO6\chels <dengsha363618@gmail.com>
2025-01-17 15:54:49 +1030

    MoocUI: 1.0.8: Della - Add DropDown Menu in TopBar's personalcon click

--- src/components/mainlayout/topbar.jsx
+++ src/components/mainlayout/topbar.jsx
@@ -1,12 +1,34 @@
-import React from "react";
-import { Box, IconButton, InputBase } from "@mui/material";
+import React, { useState } from "react";
+import { Box, IconButton, Menu, MenuItem } from "@mui/material";
 import colors from "../../theme";
-import SearchIcon from "@mui/icons-material/Search";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+
 export default function Topbar() {
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const isMenuOpen = Boolean(anchorEl);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleProfileClick = () => {
+    console.log('Profile clicked'); //should open profile route
+    handleMenuClose();
+  }
+
+  const handleLogoutClick = () => {
+    console.log('Logout clicked'); // should clear JWT token
+    handleMenuClose();
+  }
+
   return (
     <Box display="flex" justifyContent="space-between" p={2}>
       <Box
@@ -27,9 +49,27 @@ export default function Topbar() {
         <IconButton>
           <SettingsOutlinedIcon />
         </IconButton>
-        <IconButton>
+        <IconButton onClick={handleMenuOpen}>
           <PersonOutlinedIcon />
         </IconButton>
+
+        {/* Dropdown menu */}
+        <Menu
+          anchorEl={anchorEl}
+          open={isMenuOpen}
+          onClose={handleMenuClose}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+        >
+          <MenuItem onClick={handleProfileClick}>Profile</MenuItem>
+          <MenuItem onClick={handleLogoutClick}>Logout</MenuItem>
+        </Menu>
       </Box>
     </Box>
   );
