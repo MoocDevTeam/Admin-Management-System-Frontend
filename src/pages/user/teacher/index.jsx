d8f8b2c6607b0e3efa15213baf228dc946e0f170
Yafei Deng <weihaianni@gmail.com>
2025-01-19 16:16:04 +1100

    MoocUI 1.0.4: Yafei Fixed teacher list colunm display, add Username column

--- src/pages/user/teacher/index.jsx
+++ src/pages/user/teacher/index.jsx
@@ -4,6 +4,8 @@ import TeacherList from "./teacherList";
 import React,{ useState, useEffect } from "react";
 import { TocTwoTone } from "@mui/icons-material";
 import getRequest from "../../../request/getRequest";
+import { preconnect } from "react-dom";
+import colors from "../../../theme";
 
 export default function Teacher() {
   //set default page size
@@ -18,6 +20,18 @@ export default function Teacher() {
   //by default it is empty before fetching data
   const [pageData, setPageData] = useState({items:[], total:0});
 
+  //Set up setPaginationModel
+  const handlePaginationModel = (e) => {
+    setPageSearch((preState) => ({
+      ...preState, 
+      page: e.page + 1 , //set start page as 1 instead of 0
+      pageSize: e.pageSize, 
+    }));
+  }
+  //set default row selected to empty array
+  const [rowSelectionModel, setRowSelectionModel] = useState([]);
+
+
   //fetche data and rerender when pageSearch change
   useEffect(() => {
     let getTeacher = async (param) => {
@@ -27,7 +41,6 @@ export default function Teacher() {
       } else {
         setPageData({ items: [], total: 0 });
       }
-      console.log("Teacher resulte:", result);
     };
 
     let filterPagedResultRequestDto = {
@@ -44,7 +57,7 @@ export default function Teacher() {
   const columns = [
     { field: "id", headerName: "ID" , flex:0.5},
     {
-      field: "userName",
+      field: "displayName",
       headerName: "Name",
       flex: 1,
       cellClassName: "name-column--cell"
@@ -87,7 +100,7 @@ export default function Teacher() {
       renderCell: (params) => (params.row.isActive? "Yes":"No")
     },
     {
-      field: "createdByUerId",
+      field: "createdByUserId",
       headerName: "Created By",
       flex: 1,
       cellClassName: "createdByUerId-column--cell"
@@ -135,21 +148,59 @@ export default function Teacher() {
     <Box m="20px">
       
       <Header title="Teacher" subtitle="Manging Teacher Members"></Header>
-      <Box>
-        <Box>
+      <Box
+       m="40px 0 0 0"
+       minHeight={'500px'}
+       height="100%"
+       sx={{
+         "& .MuiDataGrid-root": {
+           border: "none",
+         },
+         "& .MuiDataGrid-cell": {
+           borderBottom: "none",
+         },
+         "& .name-column--cell": {
+           color: colors.greenAccent[300],
+         },
+         "& .MuiDataGrid-columnHeaders": {
+           backgroundColor: colors.blueAccent[700],
+           borderBottom: "none",
+         },
+         "& .MuiDataGrid-virtualScroller": {
+           backgroundColor: colors.primary[400],
+         },
+         "& .MuiDataGrid-footerContainer": {
+           borderTop: "none",
+           backgroundColor: colors.blueAccent[700],
+         },
+         "& .MuiCheckbox-root": {
+           color: `${colors.greenAccent[200]} !important`,
+         },
+       }}
+      >
+        <Box sx={{ mb: "15px" }}>
+          <Stack direction="row" spacing={2} justifyContent="flex-end">
+
           <Button variant="contained">
             ADD TEACHER
           </Button>
+
           <Button color="secondary" variant="contained">
             DELETE
-            </Button>
+          </Button>
+          
+          </Stack>
+          
         </Box>
+        <TeacherList
+          columns={columns}
+          pageData={pageData}
+          setPaginationModel={handlePaginationModel}
+          setRowSelectionModel={setRowSelectionModel}
+      ></TeacherList>
 
       </Box>
-      <TeacherList
-        columns={columns}
-        pageData={pageData}
-      ></TeacherList>
+
       
     </Box>
   );
