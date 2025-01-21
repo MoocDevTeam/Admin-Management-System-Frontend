614c8cfc99b0e6833c86a37018d7f8c34878759d
Laura <laurajiao7@gmail.com>
2025-01-21 14:59:32 +1100

    feature:added category page and its related function

---
+++ src/pages/course/category/categoryList.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Typography } from "@mui/material";
+import Skeleton from "@mui/material/Skeleton";
+import FlexList from "../../../components/course/category/FlexList";
+import CategoryCard from "../../../components/course/category/CategoryCard";
+
+export default function CategoryList({ currentCategories, handleCategoryClick, loading, error }) {
+    
+  if (loading) {
+    return (
+      <FlexList>
+        {Array.from({ length: 6 }).map((_, index) => (
+          <Skeleton key={index} variant="rounded" width={300} height={100} />
+        ))}
+      </FlexList>
+    );
+  }
+
+  if (error) {
+    return <Typography sx={{ marginBottom: 4 }}>{error}</Typography>;
+  }
+
+  if (currentCategories.length === 0) {
+    return (
+      <Typography align="center" sx={{ marginBottom: 4 }}>
+        No category available at the moment.
+      </Typography>
+    );
+  }
+
+return(
+<FlexList>
+  {currentCategories.map((category) => (
+    <CategoryCard
+      key={category.id}
+      categoryId={category.id}
+      categoryName={category.categoryName}
+      description={category.description || "No description available"}
+      imageUrl={category.iconUrl}
+      subCategoryCounts={category.childrenCategories?.length || 0}
+      onClick={() => handleCategoryClick(category)}
+    />
+  ))}
+</FlexList>
+   )
+}
