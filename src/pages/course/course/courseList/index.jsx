4e55eb19bf79b15d2b7a00e1ff8806f0e7860d2a
Yang Liu <69631635+changzzzzz@users.noreply.github.com>
2025-01-23 09:08:20 +1100

    Refactoring Search bar and chip

--- src/pages/course/course/courseList/index.jsx
+++ src/pages/course/course/courseList/index.jsx
@@ -1,79 +1,32 @@
 import React, { useState, useEffect } from "react";
-import Header from "../../../../components/header";
-import getRequest from "../../../../request/getRequest";
 import {
   Box,
   Button,
   Chip,
-  FormControl,
-  IconButton,
-  InputAdornment,
-  InputLabel,
-  MenuItem,
-  Modal,
-  OutlinedInput,
-  Stack,
   TextField,
   Typography,
+  Stack,
+  Skeleton,
 } from "@mui/material";
 import { Link } from "react-router-dom";
-import FlexList from "../../../../components/course/course/FlexList";
-import CourseCard from "../../../../components/course/course/CourseCard";
-import Skeleton from "@mui/material/Skeleton";
-import toast from "react-hot-toast";
-import postRequest from "../../../../request/postRequest";
 import { useDispatch, useSelector } from "react-redux";
-import { setCourses, filterCourses } from "../../../../store/courseSlice";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { setCourses } from "../../../../store/courseSlice";
+import getRequest from "../../../../request/getRequest";
+import CourseCard from "../../../../components/course/course/CourseCard";
+import FlexList from "../../../../components/course/course/FlexList";
 
-import FilterDropdown from "../../../../components/course/course/FilterDropdown";
-import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
-import RssFeedRoundedIcon from "@mui/icons-material/RssFeedRounded";
-import { createSlice } from "@reduxjs/toolkit";
 export default function CourseList() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
-  const [open, setOpen] = useState(false);
   const [selectedChip, setSelectedChip] = useState(null);
   const [categories, setCategories] = useState([]);
   const [filteredCourses, setFilteredCourses] = useState([]);
-  const [selectCatetory, setSelectCatetory] = useState("");
-  const [newCourseData, setNewCourseData] = useState({
-    title: "",
-    courseCode: "",
-    coverImage: "",
-    description: "",
-    categoryId: 1,
-  });
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
-  const handleChange = (event) => {
-    setNewCourseData({
-      ...newCourseData,
-      [event.target.name]: event.target.value,
-    });
-    console.log("newCourseData", newCourseData);
-  };
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const response = await postRequest("/MoocCourse/add", newCourseData);
-      if (response.isSuccess) {
-        toast.success("Course added successfully!");
-        setCourses([...courses, response.data]);
-        handleClose();
-      } else {
-        toast.error(response.message || "Failed to add course.");
-      }
-    } catch (error) {
-      toast.error("Failed to add course.");
-    }
-  };
+  const [searchQuery, setSearchQuery] = useState("");
 
   const dispatch = useDispatch();
   const courses = useSelector((state) => state.course.filteredCourses);
 
+  // Fetch data from backend
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -84,11 +37,8 @@ export default function CourseList() {
         );
         if (response?.isSuccess) {
           dispatch(setCourses(response?.data));
-          setFilteredCourses(response?.data); // initializing course list
+          setFilteredCourses(response?.data);
 
-          // console.log("all courses", response?.data);
-
-          //show category names
           const uniqueCategories = [];
           response?.data.forEach((course) => {
             if (
@@ -102,85 +52,81 @@ export default function CourseList() {
               });
             }
           });
-
-          setCategories(uniqueCategories); // Update the component's state
-          console.log("categories", categories);
+          setCategories(uniqueCategories);
           setError("");
         } else {
-          const errorMessage =
-            response?.message || "An error occurred while fetching data.";
-          setError(errorMessage);
-          toast.error(errorMessage);
+          setError(
+            response?.message || "An error occurred while fetching data."
+          );
         }
       } catch (err) {
         setError("Failed to fetch data");
-        toast.error("Failed to fetch data");
       }
     };
-
     fetchData();
   }, [dispatch]);
 
+  // Handle chip for choosing category
   const handleChipClick = (chipIndex) => {
     setSelectedChip(chipIndex);
-    let filteredCourses = [];
-    if (chipIndex === null) {
-      filteredCourses = courses; //  "All Courses" show all courses
-    } else {
-      const selectedCategoryName = categories[chipIndex]?.categoryName;
-      filteredCourses = courses.filter(
-        (course) => course.categoryName === selectedCategoryName
-      );
-    }
-    setFilteredCourses(filteredCourses); // Update local state
+    const filtered =
+      chipIndex === null // All course
+        ? courses
+        : courses.filter(
+            (course) =>
+              course.categoryName === categories[chipIndex]?.categoryName
+          );
+    setFilteredCourses(filtered);
+  };
+
+  // Search bar
+  const handleSearchChange = (event) => {
+    const query = event.target.value.toLowerCase();
+    setSearchQuery(query);
+    const filtered = courses.filter(
+      (course) =>
+        course.title.toLowerCase().includes(query) ||
+        course.categoryName.toLowerCase().includes(query)
+    );
+    setFilteredCourses(filtered);
   };
 
   return (
     <Box m="20px">
-      {/* Title */}
-      <Header title="Courses" subtitle="Managing all courses" />
+      <Typography variant="h4" gutterBottom>
+        Manage Courses
+      </Typography>
 
-      {/* Filter chips */}
-      <Box
-        sx={{
-          display: "inline-flex",
-          flexDirection: "row",
-          gap: 3,
-          overflow: "auto",
-          marginBottom: 2,
-        }}
-      >
-        <Chip
-          onClick={() => handleChipClick(null)}
-          size="medium"
-          label="All Courses"
-          variant={selectedChip == null ? "filled" : "outlined"}
-        />
-        {categories.map((category, index) => (
+      {/* Chips and Search Bar */}
+      <Box display="flex" alignItems="center" mb={2}>
+        <Box
+          sx={{ display: "flex", gap: 2, overflow: "auto", flexWrap: "wrap" }}
+        >
           <Chip
-            key={category.id || index}
-            label={category.categoryName}
-            variant={selectedChip === index ? "filled" : "outlined"}
-            onClick={() => handleChipClick(index)}
-            sx={{ marginRight: 1 }}
+            onClick={() => handleChipClick(null)}
+            label="All Courses"
+            variant={selectedChip == null ? "filled" : "outlined"}
           />
-        ))}
+          {categories.map((category, index) => (
+            <Chip
+              key={category.id}
+              label={category.categoryName}
+              onClick={() => handleChipClick(index)}
+              variant={selectedChip === index ? "filled" : "outlined"}
+            />
+          ))}
+        </Box>
+        <TextField
+          variant="outlined"
+          size="small"
+          placeholder="Search by title or category"
+          value={searchQuery}
+          onChange={handleSearchChange}
+          sx={{ ml: 2, width: "300px" }}
+        />
       </Box>
 
-      {/* Filter button and Add course button */}
-      <Stack
-        direction="row"
-        justifyContent="flex-end"
-        alignItems="center"
-        spacing={2}
-      >
-        {/* <FilterDropdown /> */}
-        <Button variant="contained" color="secondary" onClick={handleOpen}>
-          Add Course
-        </Button>
-      </Stack>
-
-      {/* Course card list */}
+      {/* Course List */}
       {loading && (
         <FlexList>
           {Array.from({ length: 6 }).map((_, index) => (
@@ -189,155 +135,30 @@ export default function CourseList() {
         </FlexList>
       )}
 
-      {error && <Typography sx={{ marginBottom: 4 }}>{error}</Typography>}
+      {error && <Typography color="error">{error}</Typography>}
 
-      {/* Error message of Course card */}
-      {!loading && !error && courses.length === 0 && (
-        <Typography align="center" sx={{ marginBottom: 4 }}>
-          No courses available at the moment.
-        </Typography>
+      {!loading && !error && filteredCourses.length === 0 && (
+        <Typography>No courses found.</Typography>
       )}
 
-      {!loading && !error && courses.length > 0 && (
-        <FlexList
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "stretch",
-          }}
-        >
-          {" "}
-          {filteredCourses.map((course, index) => {
-            return (
-              // To courseSingle
-              <Link
-                key={course.courseCode}
-                to={`/course/${course.id}`}
-                style={{ textDecoration: "none", display: "flex" }}
-              >
-                <CourseCard
-                  title={course.title}
-                  category={`Categories: ${course.categoryName || "N/A"}`}
-                  description={`Description: ${
-                    course.description || "No description available"
-                  }`}
-                  imageUrl={course.coverImage}
-                />
-              </Link>
-            );
-          })}
+      {!loading && !error && filteredCourses.length > 0 && (
+        <FlexList>
+          {filteredCourses.map((course) => (
+            <Link
+              key={course.id}
+              to={`/course/${course.id}`}
+              style={{ textDecoration: "none", display: "flex" }}
+            >
+              <CourseCard
+                title={course.title}
+                category={`Category: ${course.categoryName || "N/A"}`}
+                description={course.description || "No description available"}
+                imageUrl={course.coverImage}
+              />
+            </Link>
+          ))}
         </FlexList>
       )}
-
-      {/* Popup add course page */}
-      <Modal open={open} onClose={handleClose}>
-        <Box
-          sx={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            width: 400,
-            bgcolor: "background.paper",
-            boxShadow: 24,
-            p: 4,
-          }}
-        >
-          <Typography variant="h6" component="h2" marginBottom={2}>
-            Add New Course
-          </Typography>
-          <form onSubmit={handleSubmit}>
-            {/* <TextField
-              label="ID"
-              name="id"
-              value={newCourseData.id}
-              onChange={handleChange}
-              fullWidth
-              margin="normal"
-            /> */}
-            <TextField
-              label="Title"
-              name="title"
-              value={newCourseData.title}
-              onChange={handleChange}
-              fullWidth
-              margin="normal"
-            />
-            <TextField
-              label="Course Code"
-              name="courseCode"
-              value={newCourseData.courseCode}
-              onChange={handleChange}
-              fullWidth
-              margin="normal"
-            />
-            <TextField
-              label="Cover Image URL"
-              name="coverImage"
-              value={newCourseData.coverImage}
-              onChange={handleChange}
-              fullWidth
-              margin="normal"
-            />
-            <FormControl fullWidth margin="normal">
-              <InputLabel id="category-select-label">Category</InputLabel>
-              <Select
-                labelId="category-select-label"
-                name="categoryId"
-                value={newCourseData.categoryId}
-                onChange={(event) =>
-                  setNewCourseData({
-                    ...newCourseData,
-                    categoryId: event.target.value,
-                  })
-                }
-              >
-                {categories.map((category) => (
-                  <MenuItem key={category.id} value={category.id}>
-                    {category.categoryName}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-
-            <TextField
-              label="Description"
-              name="description"
-              value={newCourseData.description}
-              onChange={handleChange}
-              fullWidth
-              multiline
-              rows={4}
-              margin="normal"
-            />
-
-            <Button type="submit" variant="contained" color="primary">
-              Save
-            </Button>
-          </form>
-        </Box>
-      </Modal>
     </Box>
-  );
-}
-
-export function Search() {
-  return (
-    <FormControl sx={{ width: { xs: "100%", md: "25ch" } }} variant="outlined">
-      <OutlinedInput
-        size="small"
-        id="search"
-        placeholder="Search…"
-        sx={{ flexGrow: 1 }}
-        startAdornment={
-          <InputAdornment position="start" sx={{ color: "text.primary" }}>
-            <SearchRoundedIcon fontSize="small" />
-          </InputAdornment>
-        }
-        inputProps={{
-          "aria-label": "search",
-        }}
-      />
-    </FormControl>
   );
 }
