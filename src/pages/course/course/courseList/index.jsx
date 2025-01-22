dd3ff3a1920f569443802b002947c140f5cc895c
Yang Liu <69631635+changzzzzz@users.noreply.github.com>
2025-01-23 09:49:58 +1100

    MoocUI 1.0.12 Yang: Refactor Courselist, add search by title and comment

--- src/pages/course/course/courseList/index.jsx
+++ src/pages/course/course/courseList/index.jsx
@@ -7,14 +7,20 @@ import {
   Typography,
   Stack,
   Skeleton,
+  Modal,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import postRequest from "../../../../request/postRequest";
+import toast from "react-hot-toast";
 import { setCourses } from "../../../../store/courseSlice";
 import getRequest from "../../../../request/getRequest";
 import CourseCard from "../../../../components/course/course/CourseCard";
 import FlexList from "../../../../components/course/course/FlexList";
-
 export default function CourseList() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
@@ -22,11 +28,52 @@ export default function CourseList() {
   const [categories, setCategories] = useState([]);
   const [filteredCourses, setFilteredCourses] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
+  const [open, setOpen] = useState(false);
+  const [newCourseData, setNewCourseData] = useState({
+    title: "",
+    courseCode: "",
+    coverImage: "",
+    description: "",
+    categoryId: 1,
+  });
+
+  // Pop up page for adding course
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  // handle add course form
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setNewCourseData({
+      ...newCourseData,
+      categoryId: value,
+      // [event.target.name]: event.target.value,
+      [name]: name === "categoryId" ? Number(value) : value,
+    });
+    console.log("newCourseData", newCourseData);
+  };
+
+  // Submit add course form
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await postRequest("/MoocCourse/add", newCourseData);
+      if (response.isSuccess) {
+        toast.success("Course added successfully!");
+        setCourses([...courses, response.data]);
+        handleClose();
+      } else {
+        toast.error(response.message || "Failed to add course.");
+      }
+    } catch (error) {
+      toast.error("Failed to add course.");
+    }
+  };
 
   const dispatch = useDispatch();
   const courses = useSelector((state) => state.course.filteredCourses);
 
-  // Fetch data from backend
+  // Fetch data from back end when loading page
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -38,7 +85,7 @@ export default function CourseList() {
         if (response?.isSuccess) {
           dispatch(setCourses(response?.data));
           setFilteredCourses(response?.data);
-
+          // store category name and id in list
           const uniqueCategories = [];
           response?.data.forEach((course) => {
             if (
@@ -51,6 +98,7 @@ export default function CourseList() {
                 categoryName: course.categoryName,
               });
             }
+            console.log("categories", categories);
           });
           setCategories(uniqueCategories);
           setError("");
@@ -66,11 +114,11 @@ export default function CourseList() {
     fetchData();
   }, [dispatch]);
 
-  // Handle chip for choosing category
+  // Chip for choosing category
   const handleChipClick = (chipIndex) => {
     setSelectedChip(chipIndex);
     const filtered =
-      chipIndex === null // All course
+      chipIndex === null
         ? courses
         : courses.filter(
             (course) =>
@@ -79,7 +127,7 @@ export default function CourseList() {
     setFilteredCourses(filtered);
   };
 
-  // Search bar
+  // Search by course name
   const handleSearchChange = (event) => {
     const query = event.target.value.toLowerCase();
     setSearchQuery(query);
@@ -94,7 +142,7 @@ export default function CourseList() {
   return (
     <Box m="20px">
       <Typography variant="h4" gutterBottom>
-        Manage Courses
+        Courses
       </Typography>
 
       {/* Chips and Search Bar */}
@@ -119,12 +167,25 @@ export default function CourseList() {
         <TextField
           variant="outlined"
           size="small"
-          placeholder="Search by title or category"
+          placeholder="Search by Course Title"
           value={searchQuery}
           onChange={handleSearchChange}
-          sx={{ ml: 2, width: "300px" }}
+          sx={{ ml: 2, width: 300 }}
         />
       </Box>
+
+      {/* Filter button and Add course button */}
+      <Stack
+        direction="row"
+        justifyContent="flex-end"
+        alignItems="center"
+        spacing={2}
+      >
+        {/* <FilterDropdown /> */}
+        <Button variant="contained" color="secondary" onClick={handleOpen}>
+          Add Course
+        </Button>
+      </Stack>
 
       {/* Course List */}
       {loading && (
@@ -159,6 +220,82 @@ export default function CourseList() {
           ))}
         </FlexList>
       )}
+
+      {/* Pop up add course page */}
+      <Modal open={open} onClose={handleClose}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 400,
+            bgcolor: "background.paper",
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <Typography variant="h6" component="h2" marginBottom={2}>
+            Add New Course
+          </Typography>
+          <form onSubmit={handleSubmit}>
+            <TextField
+              label="Title"
+              name="title"
+              value={newCourseData.title}
+              onChange={handleChange}
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              label="Course Code"
+              name="courseCode"
+              value={newCourseData.courseCode}
+              onChange={handleChange}
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              label="Cover Image URL"
+              name="coverImage"
+              value={newCourseData.coverImage}
+              onChange={handleChange}
+              fullWidth
+              margin="normal"
+            />
+            <FormControl fullWidth margin="normal">
+              <InputLabel id="category-select-label">Category</InputLabel>
+              <Select
+                labelId="category-select-label"
+                id="category-select"
+                name="categoryId"
+                value={newCourseData.categoryId}
+                onChange={handleChange}
+                label="Category"
+              >
+                {categories.map((category) => (
+                  <MenuItem key={category.categoryId} value={category.id}>
+                    {category.categoryName}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <TextField
+              label="Description"
+              name="description"
+              value={newCourseData.description}
+              onChange={handleChange}
+              fullWidth
+              multiline
+              rows={4}
+              margin="normal"
+            />
+            <Button type="submit" variant="contained" color="primary">
+              Save
+            </Button>
+          </form>
+        </Box>
+      </Modal>
     </Box>
   );
 }
