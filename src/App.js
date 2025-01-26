176470d2a287e7deafb91efdba02ac860aca8680
michaelchen943 <michael.chen394@aliyun.com>
2025-01-26 17:35:30 +0800

    MoocApi 1.0.15 Michael:comment out user and add user code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect, Suspense } from "react";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/dashboard";
-import Mainlayout from "./components/mainlayout/index";
-import { Toaster } from "react-hot-toast";
-import User from "./pages/user/index";
-import AddUser from "./pages/user/addUser";
-import Role from "./pages/role/index";
-import Teacher from "./pages/user/teacher/index";
-import CourseList from "./pages/course/course/courseList/index";
-import CourseSingle from "./pages/course/course/courseSingle/index";
-import CourseInstanceSingle from "./pages/course/course/courseInstanceSingle";
-import Category from "./pages/course/category/index";
-import CourseLaunch from "./pages/course/courseLaunch/index";
-import Page404 from "./pages/page404";
-import { theme } from "./theme";
-import LoginPage from "./pages/login/login";
-import UserProfile from "./pages/profile/userProfile";
+import React, { useState, useEffect, Suspense } from "react"
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import { Routes, Route } from "react-router-dom"
+import Dashboard from "./pages/dashboard"
+import Mainlayout from "./components/mainlayout/index"
+import { Toaster } from "react-hot-toast"
+// import User from "./pages/user/index"
+// import AddUser from "./pages/user/addUser"
+import Role from "./pages/role/index"
+import Teacher from "./pages/user/teacher/index"
+import CourseList from "./pages/course/course/courseList/index"
+import CourseSingle from "./pages/course/course/courseSingle/index"
+import CourseInstanceSingle from "./pages/course/course/courseInstanceSingle"
+import Category from "./pages/course/category/index"
+import CourseLaunch from "./pages/course/courseLaunch/index"
+import Page404 from "./pages/page404"
+import { theme } from "./theme"
+import LoginPage from "./pages/login/login"
+import UserProfile from "./pages/profile/userProfile"
 
 function App() {
   return (
@@ -27,9 +27,9 @@ function App() {
         <Route Key="login" path="/login" element={<LoginPage />} />
         <Route key="home" path="/" element={<Mainlayout />}>
           <Route key="dashboard" path="/" element={<Dashboard />} />
-          <Route key="user" path="/user" element={<User />} />
+          {/* <Route key="user" path="/user" element={<User />} /> */}
           <Route key="role" path="/role" element={<Role />} />
-          <Route key="addUser" path="/user/add" element={<AddUser />} />
+          {/* <Route key="addUser" path="/user/add" element={<AddUser />} /> */}
           <Route key="profile" path="/profile" element={<UserProfile />} />
           <Route key="courseList" path="/course" element={<CourseList />} />
 
@@ -57,13 +57,7 @@ function App() {
             element={<CourseInstanceSingle />}
           />
 
-          <Route
-            key="teacher"
-            path="/user/teacher"
-            element={
-              <Teacher />
-            }
-          />
+          <Route key="teacher" path="/user/teacher" element={<Teacher />} />
 
           <Route key="page404" path="*" element={<Page404 />} />
         </Route>
