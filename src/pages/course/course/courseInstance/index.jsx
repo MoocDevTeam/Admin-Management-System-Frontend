3a839666b8169e3ad44e001f5ccb32e6db057efb
Yafei Deng <weihaianni@gmail.com>
2025-02-07 01:35:45 +1100

    MoocUI 1.0.31 Yafei: fix courseInstance index.js import issue

--- src/pages/course/course/courseInstance/index.jsx
+++ src/pages/course/course/courseInstance/index.jsx
@@ -25,7 +25,7 @@ import postRequest from "../../../../request/postRequest";
 import colors from "../../../../theme";
 import { useDispatch, useSelector } from "react-redux";
 import { setCurrentCategories } from "../../../../store";
-import EditCourseModal from "../editCourse";
+import EditCourseModal from "../../../../components/course/course/editCourse";
 
 function useCourse(courseId) {
   return useQuery(["course", courseId], () =>
