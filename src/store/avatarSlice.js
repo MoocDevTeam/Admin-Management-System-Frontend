8ac567a7876bfe1cc94b5f8193af578c213e0dbd
DESKTOP-LLHTIO6\chels <dengsha363618@gmail.com>
2025-02-14 13:42:59 +1030

    MoocUI: 1.0.35: Della - Add avatarSlice and getaAvatarByUserName funtion
    - Add `avatarSlice` and then add the getAvatarByUserName function to make the sidebar's avatar can be displayed.
    - Change version into 1.0.35

---
+++ src/store/avatarSlice.js
@@ -0,0 +1,47 @@
+// getAvatarById
+
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import getRequest from "../request/getRequest";
+
+export const getAvatarByUserName = createAsyncThunk(
+    "avatar/getAvatarByUserName",
+    async (userName, { rejectWithValue }) => {
+        const response = await getRequest(`/Avatar/GetAvatar/${userName}`);
+        if (response.avatarUrl !== null) {
+            return response;
+        }
+        return rejectWithValue(response.message);
+    }
+);
+
+const avatarSlice = createSlice({
+    name: "avatar",
+    initialState: {
+        avatar: {
+            avatarUrl: "",
+        },
+        status: "idle",
+        error: null,
+    },
+    reducer: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(getAvatarByUserName.pending, (state) => {
+                state.avatarStatus = "loading";
+                state.error = null;
+                console.log("Avatar loading")
+            })
+            .addCase(getAvatarByUserName.fulfilled, (state, action) => {
+                state.avatarStatus = "succeeded";
+                state.avatar = action.payload;
+                console.log("Avatar success")
+            })
+            .addCase(getAvatarByUserName.rejected, (state, action) => {
+                state.avatarStatus = "failed";
+                state.error = action.payload;
+                console.log("Avatar failed");
+            });
+    },
+});
+
+export default avatarSlice.reducer;
